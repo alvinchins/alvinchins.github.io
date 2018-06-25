60bf0bb50585bde09a03f3dd41de581be5ed19b7
Alvin J <alvinchins@gmail.com>
2018-06-25 17:13:22 +1000

    json test

--- jscript.js
+++ jscript.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     $("button").click(function(){
         $.getJSON("data.js", function(result){
             $.each(result, function(i, field){
-                $(".footer").append(field + " ");
+            	$.each(field,function(j,value))
+                $(".footer").append(value + " ");
             });
         });
     });
