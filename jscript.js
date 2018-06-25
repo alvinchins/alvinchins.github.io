518db9f7a992c6c398005b6a520056b97936d392
Alvin J <alvinchins@gmail.com>
2018-06-25 17:32:33 +1000

    json structure test

--- jscript.js
+++ jscript.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
     $("button").click(function(){
-        $.getJSON("data.js", function(result){
-            $.each(result, function(i, field){
-            	$.each(field,function(j,value))
-                $(".footer").append(value + " ");
-            });
+        $.getJSON("data.js", function(works){
+            $.works.sites, function(i,site){
+            	alert(site.name);
+            }
         });
     });
 });
