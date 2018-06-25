9f1a73daacf5e36a62f776e8f01f4dfa3dd8c139
Alvin J <alvinchins@gmail.com>
2018-06-25 17:09:06 +1000

    selector fix

--- jscript.js
+++ jscript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("button").click(function(){
         $.getJSON("data.js", function(result){
             $.each(result, function(i, field){
-                $("div").append(field + " ");
+                $(".footer").append(field + " ");
             });
         });
     });
