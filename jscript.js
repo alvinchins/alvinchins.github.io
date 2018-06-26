6911a8a127d03485d147fb67978a85fa5708637d
Alvin J <alvinchins@gmail.com>
2018-06-26 10:28:47 +1000

    json test

--- jscript.js
+++ jscript.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     $("button").click(function(){
         $.getJSON("data.js", function(works){
-        	console.log(works);
-            $.each(works,function(work,project){
-            	console.log(work);
-            	console.log(project);
-            	console.log(project.name);
+            $.each(works,function(work,projects){
+            	console.log(projects);
+            	$.each(projects,function(i,project){
+            		console.log(project);
+            		alert(project.name);
+            	});
             });
         });
     });
