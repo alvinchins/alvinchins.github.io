32ae8718bace8304edc8f7b3ce1e9d32f06588bd
Alvin J <alvinchins@gmail.com>
2018-06-26 15:22:15 +1000

    html template test

--- jscript.js
+++ jscript.js
@@ -2,19 +2,11 @@ $(document).ready(function(){
     $("button").click(function(){
         $.getJSON("data.js", function(works){
             $.each(works,function(work,projects){
-            	var currentSites = document.createElement("div");
-            	currentSites.setAttribute("class","row");
+            	var currentSites = document.getElementByTagName("template")[0];
             	$.each(projects,function(i,project){
-            		var siteWraper = document.createElement("div");
-            		var siteTitle = document.createElement("h1");
-            		siteTitle.innerHTML = project.name;
-            		var siteDomain = document.createElement("h3");
-            		siteDomain.innerHTML = project.domain;
-            		siteWraper.appendChild(siteTitle);
-            		siteWraper.appendChild(siteDomain);
-            		currentSites.appendChild(siteWraper);
+            		
             	});
-            	$("body").append(currentSites);
+            	$("#mySites").append(currentSites);
             });
         });
     });
