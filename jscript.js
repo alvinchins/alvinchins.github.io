9d2817cee7042c83b1424d1387384c24e684fff8
Alvin J <alvinchins@gmail.com>
2018-06-26 15:29:13 +1000

    temp test add div

--- jscript.js
+++ jscript.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
     $("button").click(function(){
         $.getJSON("data.js", function(works){
             $.each(works,function(work,projects){
-            	var currentSites = document.getElementsByTagName("template")[0];
+            	var template = document.getElementsByTagName("template")[0];
             	$.each(projects,function(i,project){
+            		currentSites = document.importNode(template,true);
             		
             	});
             	$("#mySites").append(currentSites);
