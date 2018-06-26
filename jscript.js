6ec115a178bcd4c23cd58e3da6a69b01394ef841
Alvin J <alvinchins@gmail.com>
2018-06-26 10:17:54 +1000

    json test

--- jscript.js
+++ jscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("button").click(function(){
         $.getJSON("data.js", function(works){
-        	alert(works);
+        	console.log(works);
             $.each(works,function(work,project){
             	alert(project.name + project.domain);
             });
