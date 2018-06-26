2a8a4b83de0afbccea85f29afe0301eac47e990a
Alvin J <alvinchins@gmail.com>
2018-06-26 15:24:25 +1000

    typo

--- jscript.js
+++ jscript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("button").click(function(){
         $.getJSON("data.js", function(works){
             $.each(works,function(work,projects){
-            	var currentSites = document.getElementByTagName("template")[0];
+            	var currentSites = document.getElementsByTagName("template")[0];
             	$.each(projects,function(i,project){
             		
             	});
