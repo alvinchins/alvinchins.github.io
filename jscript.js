1055337556c86a52814e34e0f34e4e6337ae651c
Alvin J <alvinchins@gmail.com>
2018-06-26 17:28:09 +1000

    js template test

--- jscript.js
+++ jscript.js
@@ -2,13 +2,37 @@ $(document).ready(function(){
     $("button").click(function(){
         $.getJSON("data.js", function(works){
             $.each(works,function(work,projects){
-            	var $template = document.getElementsByTagName("template")[0];
-            	var tempContent = $template.prop('content');
             	$.each(projects,function(i,project){
-            		var currentSites = document.importNode(tempContent,true);
-
+            		var template = [
+					'<div class="col-md-4">',
+						'<div class="browserFrame">',
+							'<div class="titlebar">',
+								'<h4 class="titleDisplay">',
+								'<i class="fab fa-chrome">',
+								'</i>&nbsp;'+project.name+'&nbsp;',
+								'<i class="fas fa-times"></i>',
+								'</h4>',
+								'<i class="fas fa-times"></i>',
+								'<i class="far fa-square"></i>',
+								'<i class="fas fa-minus"></i>',
+							'</div>',
+							'<div class="addressbar">',
+								'<i class="fas fa-arrow-left"></i>&nbsp;',
+								'<i class="fas fa-arrow-right"></i>&nbsp;',
+								'<i class="fas fa-sync"></i>&nbsp;',
+								'<h5 class="addressArea">',
+								'http://'+project.domain+'/',
+								'</h5>&nbsp;&nbsp;',
+								'<i class="fas fa-ellipsis-v"></i>',
+							'</div>',
+							'<div class="contentArea">',
+								'<img src="./img/thumbnail/timberclub.JPG">',
+							'</div>',
+						'</div>',
+					'</div>'
+            		].join("\n");
+            		$("#mySites").append(template);
             	});
-            	$("#mySites").append(currentSites);
             });
         });
     });
