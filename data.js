a43df8a30053d62071635e0ec9dd05a7b0c5bb3d
Alvin J <alvinchins@gmail.com>
2018-06-27 17:38:49 +1000

    more testing data

--- data.js
+++ data.js
@@ -2,25 +2,53 @@
 	"sites":[
 	{
 		"name":"Timberclub",
-		"domain":"timberclubaustralia.com.au"
+		"domain":"timberclubaustralia.com.au",
+		"wp":"1",
+		"eco":"0",
+		"tmp":"0",
+		"nsfw":"0"
 	},{
 		"name":"THN Partners",
-		"domain":"thnpartners.com.au"
+		"domain":"thnpartners.com.au",
+		"wp":"1",
+		"eco":"0",
+		"tmp":"0",
+		"nsfw":"0"
 	},{
 		"name":"Sydney Garden Lodge",
-		"domain":"sydgardenlodge.com.au"
+		"domain":"sydgardenlodge.com.au",
+		"wp":"1",
+		"eco":"0",
+		"tmp":"0",
+		"nsfw":"0"
 	},{
 		"name":"Jessie Coffee School",
-		"domain":"jessiecoffee.com.au"
+		"domain":"jessiecoffee.com.au",
+		"wp":"1",
+		"eco":"1",
+		"tmp":"1",
+		"nsfw":"0"
 	},{
 		"name":"South Pacific Resort Hotel",
-		"domain":"southpacresort.com.au"
+		"domain":"southpacresort.com.au",
+		"wp":"1",
+		"eco":"0",
+		"tmp":"1",
+		"nsfw":"0"
 	},{
 		"name":"Asian Butterfly Massage",
-		"domain":"asianbutterflymassage.com.au"
+		"domain":"asianbutterflymassage.com.au",
+		"wp":"1",
+		"eco":"0",
+		"tmp":"0",
+		"nsfw":"1"
 	},{
 		"name":"National Car Rental",
-		"domain":"nationalcarrentals.com.au"
+		"domain":"nationalcarrentals.com.au",
+		"wp":"1",
+		"eco":"0",
+		"tmp":"0",
+		"nsfw":"0"
 	}
 	]
 }
