d55ab7ad7a0431611191797405088d77665fd3c4
Alvin J <alvinchins@gmail.com>
2018-06-27 16:00:36 +1000

    add more samples

--- data.js
+++ data.js
@@ -6,6 +6,21 @@
 	},{
 		"name":"THN Partners",
 		"domain":"thnpartners.com.au"
+	},{
+		"name":"Sydney Garden Lodge",
+		"domain":"sydgardenlodge.com.au"
+	},{
+		"name":"Jessie Coffee School",
+		"domain":"jessiecoffee.com.au"
+	},{
+		"name":"South Pacific Resort Hotel",
+		"domain":"southpacresort.com.au"
+	},{
+		"name":"Asian Butterfly Massage",
+		"domain":"asianbutterflymassage.com.au"
+	},{
+		"name":"National Car Rental",
+		"domain":"nationalcarrentals.com.au"
 	}
 	]
 }
